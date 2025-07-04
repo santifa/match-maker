9dbab201df0a392bf231b6dea291f11fa5bcebc2
Henrik Jürges <juerges.henrik@gmail.com>
2025-07-04 15:52:39 +0200

    setup production deploys

--- config/prod.exs
+++ config/prod.exs
@@ -19,3 +19,15 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :match_maker, MatchMakerWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :match_maker, MatchMaker.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
